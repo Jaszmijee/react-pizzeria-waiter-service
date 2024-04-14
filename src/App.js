2ef88c82c61968ee1f684dbbf38927e1523a86a8
jaszmije <jaszmije@gmail.com>
2024-04-14 15:03:51 +0200

    add feature add waiting message during response from server in starting App

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import NotFound from "./components/pages/NotFound/NotFound";
 import NavBar from "./components/views/NavBar/NavBar";
 import Home from "./components/pages/Home/Home";
 import Table from "./components/pages/Table/Table";
-import {useDispatch} from "react-redux";
-import {fetchTablesFromServer} from "./redux/tablesRedux";
+import {useDispatch, useSelector} from "react-redux";
+import {fetchTablesFromServer, getAllTables} from "./redux/tablesRedux";
 import {useEffect} from "react";
 
 const App = () => {
@@ -12,16 +12,22 @@ const App = () => {
     const dispatch = useDispatch();
 
     useEffect(() => dispatch(fetchTablesFromServer()), [dispatch]);
+    const tablesReady = useSelector(getAllTables);
+
+
+    if (!tablesReady.length) {
+        return <div>Loading...</div>;
+    }
 
     return (
         <main>
-      {/*<Container>*/}
-                <NavBar/>
-                <Routes>
-                    <Route path="/" element={<Home/>}/>
-                 <Route path="/table/:tableId" element={<Table/>}/>
-                    <Route path="*" element={<NotFound/>}/>
-                </Routes>
+            {/*<Container>*/}
+            <NavBar/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/table/:tableId" element={<Table/>}/>
+                <Route path="*" element={<NotFound/>}/>
+            </Routes>
             {/*</Container>*/}
         </main>
     );
